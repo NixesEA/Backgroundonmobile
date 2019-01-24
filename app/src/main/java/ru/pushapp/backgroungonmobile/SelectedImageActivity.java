c296054d2d1e6d42fb38bea5b2aac26a2af29c3b
NixesEA <ermakov.anton201133@gmail.com>
2019-01-24 16:55:53 +0300

    add send image url and setWallpaper

--- app/src/main/java/ru/pushapp/backgroungonmobile/SelectedImageActivity.java
+++ app/src/main/java/ru/pushapp/backgroungonmobile/SelectedImageActivity.java
@@ -1,25 +1,36 @@
 package ru.pushapp.backgroungonmobile;
 
+import android.app.WallpaperManager;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.URI;
 import java.util.ArrayList;
 
 import ru.pushapp.backgroungonmobile.adapters.rvAdapter;
 
-public class SelectedImageActivity extends AppCompatActivity {
+public class SelectedImageActivity extends AppCompatActivity implements View.OnClickListener {
 
     ArrayList<String> urlList = new ArrayList<>();
     ArrayList<ImageModel> imagesList = new ArrayList<>();
@@ -31,6 +42,7 @@ public class SelectedImageActivity extends AppCompatActivity {
     TextView countLikesTV;
     TextView countDownloadTV;
     RecyclerView recyclerView;
+    FloatingActionButton fab;
 
     String ImageURL;
     String imageCountLikes;
@@ -52,11 +64,12 @@ public class SelectedImageActivity extends AppCompatActivity {
         bigImageView = findViewById(R.id.big_imageview);
         countLikesTV = findViewById(R.id.count_likes);
         countDownloadTV = findViewById(R.id.count_downloads);
+        fab = findViewById(R.id.floatingActionButton);
+        fab.setOnClickListener(this);
 
         recyclerView = findViewById(R.id.similar_images_rv);
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
         recyclerView.setLayoutManager(linearLayoutManager);
-
 
 
         urlList.add("https://mobimg.b-cdn.net/pic/v2/gallery/preview/abstrakciya-fon-41314.jpg");
@@ -70,7 +83,7 @@ public class SelectedImageActivity extends AppCompatActivity {
         urlList.add("https://mobimg.b-cdn.net/pic/v2/gallery/preview/abstrakciya-fon-40418.jpg");
 
 
-        for (String itemURL: urlList) {
+        for (String itemURL : urlList) {
             ImageModel imageModel = new ImageModel();
 
             imageModel.setUrl(itemURL);
@@ -102,7 +115,7 @@ public class SelectedImageActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater=getMenuInflater();
+        MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.selected_image_menu, menu);
 
         return super.onCreateOptionsMenu(menu);
@@ -110,16 +123,41 @@ public class SelectedImageActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()){
+        switch (item.getItemId()) {
             case R.id.menu_add_image_in_favor:
-                Toast.makeText(this,"Image added in favorites",Toast.LENGTH_LONG).show();
+                Toast.makeText(this, "Image added in favorites", Toast.LENGTH_LONG).show();
                 break;
             case R.id.menu_share_image:
-                Toast.makeText(this,"Try sharing",Toast.LENGTH_LONG).show();
+                sharingImage();
+//                Toast.makeText(this, "Try sharing", Toast.LENGTH_LONG).show();
                 break;
-                default:finish();
+            default:
+                finish();
         }
         return super.onOptionsItemSelected(item);
     }
 
+    void sharingImage() {
+        Intent shareIntent = new Intent(Intent.ACTION_SEND);
+        shareIntent.setType("message/rfc822");
+        shareIntent.putExtra(Intent.EXTRA_TEXT, ImageURL);
+        startActivity(Intent.createChooser(shareIntent, "Выберите куда отправить картинку"));
+    }
+
+    @Override
+    public void onClick(View view) {
+        WallpaperManager myWallpaperManager = WallpaperManager.getInstance(getApplicationContext());
+
+        bigImageView.buildDrawingCache();
+        try {
+            Toast.makeText(this, "Обои устанавливаются", Toast.LENGTH_LONG).show();
+            myWallpaperManager.setBitmap(bigImageView.getDrawingCache());
+            Toast.makeText(this, "Обои успешно установлены", Toast.LENGTH_LONG).show();
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            Toast.makeText(this, "Ошибка", Toast.LENGTH_LONG).show();
+            e.printStackTrace();
+        }
+
+    }
 }
